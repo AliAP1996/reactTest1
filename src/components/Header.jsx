dfa6a9907725046ed9981d69ee20a35dfb601196
Ali Alipour <Alii@example.com>
2026-05-27 01:15:41 -0700

    projectInit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <section className="header">
+      <div className="leftHeader">
+        <h4 className="text-red-400">My WebSite</h4>
+      </div>
+      <div className="rightHeader">
+        <h4>Shopping Center</h4>
+      </div>
+      <div>
+        <h1>hello</h1>
+      </div>
+      <div>hi</div>
+    </section>
+  );
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+    return (
+        <div className="navBar">
+            <p>One</p>
+            <p>Two</p>
+            <p>Three</p>
+            <p>Four</p>
+        </div>
+    )
+}
+
+export default Navbar;
